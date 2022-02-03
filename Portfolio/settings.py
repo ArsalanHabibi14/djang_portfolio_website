dbd1cb2fb994ac2f78fa6f2467a0bcd159ba4821
Arsalan Habibi <ibrahimarsalan94@gmail.com>
2022-02-04 03:35:58 +0430

    New Commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0c3%s2b_x^%b!&ogq=2&kiryk&51he9ufl85nzw5nt8h5ra#x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arsalanhabibi.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
